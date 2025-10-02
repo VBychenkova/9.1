8d2d048cc2b66ebc4f0a6f33d7439ddccb6229a3
Valeriya Trineeva <timka_@bk.ru>
2025-10-02 17:32:23 +0300

    Модуль 19. Фильтры и формы. Добавлены CRUD операции. Добавлена проверка прав доступа

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.NewsList.as_view(), name='news_list'),
     path('<int:pk>/', views.NewsDetail.as_view(), name='news_detail'),
+    path('search/', views.news_search, name='news_search'),
+
+    # Новости
+    path('create/', views.NewsCreate.as_view(), name='news_create'),
+    path('<int:pk>/edit/', views.NewsUpdate.as_view(), name='news_edit'),
+    path('<int:pk>/delete/', views.NewsDelete.as_view(), name='news_delete'),
+
+    # Статьи
+    path('articles/create/', views.ArticleCreate.as_view(), name='article_create'),
+    path('articles/<int:pk>/edit/', views.ArticleUpdate.as_view(), name='article_edit'),
+    path('articles/<int:pk>/delete/', views.ArticleDelete.as_view(), name='article_delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,18 +1,159 @@
-from django.views.generic import ListView, DetailView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
+from django.contrib import messages
+from django.shortcuts import redirect
+from .models import Post, Author, Category
+from .forms import PostForm
+from .filters import NewsFilter
 
 
 class NewsList(ListView):
     model = Post
     template_name = 'news/news_list.html'
-    context_object_name = 'news'
+    context_object_name = 'posts'  # меняем на posts
     ordering = ['-created_at']
+    paginate_by = 10
 
     def get_queryset(self):
-        return Post.objects.filter(post_type='NW')  # только новости
+        # Показываем и новости и статьи
+        return Post.objects.all()
 
 
 class NewsDetail(DetailView):
     model = Post
     template_name = 'news/news_detail.html'
     context_object_name = 'news'
+
+
+def news_search(request):
+    news_list = Post.objects.filter(post_type='NW').order_by('-created_at')
+    news_filter = NewsFilter(request.GET, queryset=news_list)
+
+    return render(request, 'news/news_search.html', {
+        'filter': news_filter,
+        'news': news_filter.qs
+    })
+
+
+class NewsCreate(LoginRequiredMixin, CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news/news_edit.html'
+    success_url = reverse_lazy('news_list')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_type = 'NW'
+
+        try:
+            # Пытаемся найти автора для текущего пользователя
+            author = Author.objects.get(user=self.request.user)
+        except Author.DoesNotExist:
+            # Если автора нет - создаем его
+            author = Author.objects.create(user=self.request.user)
+
+        post.author = author
+        return super().form_valid(form)
+
+
+class NewsUpdate(LoginRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news/news_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        # Проверяем, что пост является новостью
+        if obj.post_type != 'NW':
+            messages.error(request, "Вы можете редактировать только новости")
+            return redirect('news_list')
+
+        # Разрешаем доступ если пользователь - автор ИЛИ суперпользователь
+        if obj.author.user != request.user and not request.user.is_superuser:
+            messages.error(request, "Вы не можете редактировать эту новость")
+            return redirect('news_list')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class NewsDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'news/news_delete.html'
+    success_url = reverse_lazy('news_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        # Проверяем, что пост является новостью
+        if obj.post_type != 'NW':
+            messages.error(request, "Вы можете удалять только новости")
+            return redirect('news_list')
+
+        # Разрешаем доступ если пользователь - автор ИЛИ суперпользователь
+        if obj.author.user != request.user and not request.user.is_superuser:
+            messages.error(request, "Вы не можете удалить эту новость")
+            return redirect('news_list')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ArticleCreate(LoginRequiredMixin, CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news/article_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.post_type = 'AR'  # автоматически ставим тип "статья"
+
+        # Получаем или создаем автора для текущего пользователя
+        author, created = Author.objects.get_or_create(user=self.request.user)
+        post.author = author
+
+        return super().form_valid(form)
+
+
+class ArticleUpdate(LoginRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news/article_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        # Проверяем, что пост является статьей
+        if obj.post_type != 'AR':
+            messages.error(request, "Вы можете редактировать только статьи")
+            return redirect('news_list')
+
+        # Разрешаем доступ если пользователь - автор ИЛИ суперпользователь
+        if obj.author.user != request.user and not request.user.is_superuser:
+            messages.error(request, "Вы не можете редактировать эту статью")
+            return redirect('news_list')
+
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ArticleDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'news/article_delete.html'
+    success_url = reverse_lazy('news_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+
+        # Проверяем, что пост является статьей
+        if obj.post_type != 'AR':
+            messages.error(request, "Вы можете удалять только статьи")
+            return redirect('news_list')
+
+        # Разрешаем доступ если пользователь - автор ИЛИ суперпользователь
+        if obj.author.user != request.user and not request.user.is_superuser:
+            messages.error(request, "Вы не можете удалить эту статью")
+            return redirect('news_list')
+
+        return super().dispatch(request, *args, **kwargs)
